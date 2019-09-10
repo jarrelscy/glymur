b65585f5c1e674c9c26f4303324ef626da357629
John Evans <john.g.evans.ne@gmail.com>
2019-09-10 18:57:31 -0400

    Remove python 3.4 artifacts
    Build and test was already largely gone, but a few test fixtures tied to
    python 3.4 remained.  Mention of 3.4 removed from setup.py.

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ kwargs['install_requires'] = install_requires
 kwargs['classifiers'] = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
